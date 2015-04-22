30d8f28f36e39263c73b3af0f5d77376fe039c63
Kareem Carr <kcarr@broadinstitute.org>
2015-04-22 01:35:22 -0400

    function for cleaning out strange characters

--- somaticDB/DictUtilities.py
+++ somaticDB/DictUtilities.py
@@ -1,18 +1,21 @@
 def merge_dicts(dict1, dict2):
-	new_dict = {}
-	for key in dict1: new_dict[key] = dict1[key]
-	for key in dict2: new_dict[key] = dict2[key]
-	return new_dict
+    new_dict = {}
+    for key in dict1: new_dict[key] = dict1[key]
+    for key in dict2: new_dict[key] = dict2[key]
+    return new_dict
 
 def clean_up_lists(D):
-	for key in D:
-		if type(D[key]) == list: D[key] = D[key][0]
-	return D
+    for key in D:
+        if type(D[key]) == list: D[key] = D[key][0]
+    return D
+
+def drop_weird_characters(s):
+    return "".join(map(chr,filter(lambda k:k < 128, [ord(c) for c in s])))
 
 def stringify_dict(D):
-	for key in D:
-		if type(D[key]) == list:
-			D[key] = map(lambda s: str(s).encode('utf-8'),D[key])
-		else:
-			D[key] = str(D[key]).encode('utf-8')
-	return D
+    for key in D:
+        if type(D[key]) == list:
+            D[key] = map(lambda s: drop_weird_characters(str(s)),D[key])
+        else:
+            D[key] = drop_weird_characters(str(D[key]))
+    return D
