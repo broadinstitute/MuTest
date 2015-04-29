34d9be22e29a15da589f692f96d0acab49089e77
Kareem Carr <kcarr@broadinstitute.org>
2015-04-29 10:26:13 -0400

    .

--- somaticDB/DataGatherer.py
+++ somaticDB/DataGatherer.py
@@ -16,9 +16,9 @@ class DataGatherer:
         file = open(self.filename)
         reader = csv.DictReader(file,delimiter='\t')
 
-        print self.filename
-
         for file_data in reader:
+
+            print file_data
 
             meta_data_dict = get_entries_from_dict(file_data,
 
