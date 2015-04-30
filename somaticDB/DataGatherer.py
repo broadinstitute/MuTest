129aceff7f09eaf6f41dc326f48f5b01dab8d6c5
Kareem Carr <kcarr@broadinstitute.org>
2015-04-29 23:06:50 -0400

    add colon

--- somaticDB/DataGatherer.py
+++ somaticDB/DataGatherer.py
@@ -57,7 +57,7 @@ def main():
 
     parser.add_argument('-i','--input', help='Input file name',type=str,metavar='<input_file>', default="ALL",required=True)
     parser.add_argument('-p','--port', help='Input file name',type=int,metavar='<input_file>', default=27017)
-    parser.add_argument('-d','--demo', help='Demo mode',action=)
+    parser.add_argument('-d','--demo', help='Demo mode',action='store_true')
 
     args = parser.parse_args()
 
