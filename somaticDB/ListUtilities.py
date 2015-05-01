cddeaf1558648d44ddccf362588da0eee7ff6c81
Kareem Carr <kcarr@broadinstitute.org>
2015-05-01 10:02:36 -0400

    .

--- somaticDB/ListUtilities.py
+++ somaticDB/ListUtilities.py
@@ -1,10 +1,13 @@
-from itertools import product
+from itertools import product , chain
 
 def drop_none(y):
-	return filter(lambda x: x is not None, y)
+    return filter(lambda x: x is not None, y)
 
 def list_product(*args, **kwargs):
-	return list(product(*args, **kwargs))
+    result = list(product(*args, **kwargs))
+    result = map(chain, result)
+    result = map(list, result)
+    return result
 
 def list_product_drop_none(*args, **kwargs):
-	return map(drop_none,list_product(*args, **kwargs))
+    return map(drop_none,list_product(*args, **kwargs))
