2dbba2ec353d7119f48f799a08587c2c2ec5e757
Kareem Carr <kcarr@broadinstitute.org>
2015-07-12 00:23:09 -0400

    variant_submit code

--- SomaticDB/Actions/BamAggregator.py
+++ SomaticDB/Actions/BamAggregator.py
@@ -27,7 +27,7 @@ def BamAggregator(query, normal_bam_list_name, tumor_bam_list_name, interval_lis
 
     for record in collection.find(ast.literal_eval(query)):
 
-        print record
+        print record['project']
 
         if not record.has_key('tumor_bam'):
             print record
