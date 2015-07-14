e8a28abdf51f0bb5501c3e216d97eb47b2b313ff
Kareem Carr <kcarr@broadinstitute.org>
2015-07-14 15:12:58 -0400

    .

--- SomaticDB/Actions/AssessmentFileCreator.py
+++ SomaticDB/Actions/AssessmentFileCreator.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def create_assessment_file(tsv, results, output_file, evaluation_rules):
-    metadata = pd.read_csv(tsv)
+    metadata = pd.read_csv(tsv,'\t')
 
     evaluation_rules  = evaluation_rules.split(',')
     evaluation_rules  = map(lambda x: tuple(x.split(':')),evaluation_rules)
