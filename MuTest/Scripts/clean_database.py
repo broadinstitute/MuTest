03ce881592f9d05a7f80c3e7a7d0c67bfe04d2b0
Kareem Carr <kcarr@broadinstitute.org>
2015-07-26 23:28:00 -0400

    .

--- MuTest/Scripts/clean_database.py
+++ MuTest/Scripts/clean_database.py
@@ -4,5 +4,8 @@ def delete_all():
     variants = connect_to_mongo()
     variants.remove()
 
+    normal_normal = connect_to_mongo(collection='NormalNormalData')
+    normal_normal.remove()
+
 if __name__ == '__main__':
     delete_all()
