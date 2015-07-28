9f036c5b7d61b0dd3e6cb1cc9645731fae98d065
Kareem Carr <kcarr@broadinstitute.org>
2015-07-28 04:48:49 -0400

    .

--- EvaluateMutectAgainstMuTestSensitivity.scala
+++ EvaluateMutectAgainstMuTestSensitivity.scala
@@ -144,7 +144,7 @@ class Qscript_Mutect_with_SomaticDB extends QScript {
                    tsv: String) : String = {
 
 
-   val cmd: String = "mutest normal_normal_collector -q %s -n %s -t %s -o %s".format(query, normal_bam_list, tumor_bam_list, tsv)
+   val cmd: String = "mutest normal_normal_collector -q \"%s\" -n %s -t %s -o %s".format(query, normal_bam_list, tumor_bam_list, tsv)
 
     return(cmd)
   }
