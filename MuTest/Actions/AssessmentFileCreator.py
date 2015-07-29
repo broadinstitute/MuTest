f71901eb61b50253cc0f74af24b94951601231b0
Kareem Carr <kcarr@broadinstitute.org>
2015-07-28 23:00:02 -0400

    .

--- MuTest/Actions/AssessmentFileCreator.py
+++ MuTest/Actions/AssessmentFileCreator.py
@@ -4,7 +4,7 @@ import pandas as pd
 def create_assessment_file(tsv, results, output_file, evaluation_rules):
     metadata = pd.read_csv(tsv,'\t')
 
-    if evaluation_rules == "NN":
+    if evaluation_rules != "NN":
         evaluation_rules  = evaluation_rules.split(',')
         evaluation_rules  = map(lambda x: tuple(x.split(':')),evaluation_rules)
         evaluation_rules = dict(evaluation_rules)
