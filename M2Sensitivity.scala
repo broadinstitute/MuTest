58c6700fae4156e586bb342e9d5f72652028837d
Kareem Carr <kcarr@broadinstitute.org>
2015-09-28 08:32:30 -0400

    .

--- M2Sensitivity.scala
+++ M2Sensitivity.scala
@@ -160,7 +160,7 @@ mutest bam_aggregate [-h] -q <query>
                    metadata: String) : String = {
 
 
-   val cmd: String = "mutest bam_aggregate -q \"%s\" -n %s -t %s -i %s -f %s -m %s".format(query, normal_bam_list, tumor_bam_list, interval_list, folder, metadata)
+   val cmd: String = "mutest bam_aggregate -q \"\"%s\"\" -n %s -t %s -i %s -f %s -m %s".format(query, normal_bam_list, tumor_bam_list, interval_list, folder, metadata)
 
     return(cmd)
   }
