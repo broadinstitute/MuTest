06db192d2b82437a6b0777d86f026cb8f94add0c
Kareem Carr <kcarr@broadinstitute.org>
2015-10-04 22:47:04 -0400

    .

--- M2Sensitivity.scala
+++ M2Sensitivity.scala
@@ -211,7 +211,7 @@ mutest variant_assess -t <tsv>
                                @Output output: File) extends CommandLineFunction {
 
     override def commandLine: String = {
-      "mutest variant_assess -t %s -q \"%s\" -o %s -d %s".format(tsv, query, output,folder)
+      "ls -l" //"mutest variant_assess -t %s -q \"%s\" -o %s -d %s".format(tsv, query, output,folder)
     }
   }
 
