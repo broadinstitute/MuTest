60698b595e443c09f862c55e0e3714c2b8aa2bfa
Kareem Carr <kcarr@broadinstitute.org>
2015-10-06 17:19:33 -0400

    .

--- M2Specificity.scala
+++ M2Specificity.scala
@@ -82,7 +82,7 @@ class Qscript_Mutect_with_SomaticDB extends QScript {
 
     println(assessmentFilename.toString)
 
-    //add(new NormalNormalVariantAssessment(m2_out_files.map(x => new File(x)) ,submissionsFilename, query,project_dir,assessmentFilename))
+    add(new NormalNormalVariantAssessment(m2_out_files.map(x => new File(x)) ,submissionsFilename, query,project_dir,assessmentFilename))
 
     }
 
